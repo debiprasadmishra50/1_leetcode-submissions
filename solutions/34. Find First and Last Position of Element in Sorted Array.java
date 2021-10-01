e3fc851e5e084d02dd6cc0b46c4f253cd515b40b
Debi Prasad Mishra <61733256+debiprasadmishra50@users.noreply.github.com>
2021-10-01 20:08:35 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.java

--- solutions/34. Find First and Last Position of Element in Sorted Array.java
+++ solutions/34. Find First and Last Position of Element in Sorted Array.java
@@ -1,25 +1,52 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        
-        res[0] = indexOfFirstOccurance(nums, target);
-        res[1] = indexOfLastOccurance(nums, target);
-        
-        return res;
-    }
-    
-    private static int indexOfFirstOccurance(int[] arr, int x) {
-        int low = 0, high = arr.length-1;
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[2];
         
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (arr[mid] > x)
-                high = mid - 1;
-            else if (arr[mid] < x)
-                low = mid + 1;
-            else {
-                if (mid == 0 || arr[mid-1] != arr[mid])
-                    return mid;
-                else
-                    high = mid - 1;
+        res[0] = indexOfFirstOccurance(nums, target);
+        res[1] = indexOfLastOccurance(nums, target);
+        
+        return res;
+    }
+    
+    private static int indexOfFirstOccurance(int[] arr, int x) {
+		int low = 0, high = arr.length-1;
+		
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			
+			if (arr[mid] > x)
+				high = mid - 1;
+			else if (arr[mid] < x)
+				low = mid + 1;
+			else {
+				if (mid == 0 || arr[mid-1] != arr[mid])
+					return mid;
+				else
+					high = mid - 1;
+			}
+		}
+		
+		return -1;
+	}
+    
+    private static int indexOfLastOccurance(int[] arr, int x) {
+		int low = 0, high = arr.length - 1;
+		
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			
+			if (arr[mid] < x)
+				low = mid + 1;
+			else if (arr[mid] > x)
+				high = mid - 1;
+			else {
+				if (mid == arr.length-1 || arr[mid+1] != arr[mid])
+					return mid;
+				else
+					low = mid + 1;
+			}
+		}
+		
+		return -1;
+	}
+}
