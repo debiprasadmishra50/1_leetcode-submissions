cd4db8fc189fe7131de86f485245e47b3661e0d2
Debi Prasad Mishra <61733256+debiprasadmishra50@users.noreply.github.com>
2021-10-01 13:56:54 +0530

    Update 1333. Filter Restaurants by Vegan-Friendly, Price and Distance.java

--- solutions/1333. Filter Restaurants by Vegan-Friendly, Price and Distance.java
+++ solutions/1333. Filter Restaurants by Vegan-Friendly, Price and Distance.java
@@ -1,38 +1,55 @@
-//         Collections.sort(list, new Comp());
-//         List<Integer> res = new ArrayList();
-        
-//         for (int[] item : list) {
-//             // System.out.println(Arrays.toString(item));
-//             res.add(item[0]);
-//         }
-        
-//         return res;
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            return a[1] == b[1] ? b[0] - a[0] : b[1] - a[1];
-        });
-        for(int[] restaurant : restaurants) {
-            if((veganFriendly == 0 || veganFriendly == restaurant[2]) &&
-                restaurant[3] <= maxPrice &&
-                restaurant[4] <= maxDistance
-              ) {
-                pq.offer(restaurant);
-            }
-        }
-        List<Integer> result = new ArrayList<>();
-        while(!pq.isEmpty()) {
-            result.add(pq.poll()[0]);
-        }
-        return result;
-    }
-    
+class Solution {
+    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+//         List<int[]> list = new ArrayList();
+//         int n = restaurants.length;
+        
+//         for (int i = 0; i < n; i++) {
+//             if (veganFriendly == 1) {
+//                 if (restaurants[i][2] == 1 && restaurants[i][3] <= maxPrice && restaurants[i][4] <= maxDistance)
+//                     list.add(restaurants[i]);
+//             }
+//             else {
+//                 if (restaurants[i][3] <= maxPrice && restaurants[i][4] <= maxDistance)
+//                     list.add(restaurants[i]);
+//             }
+//         }
+        
+        
+//         Collections.sort(list, new Comp());
+//         List<Integer> res = new ArrayList();
+        
+//         for (int[] item : list) {
+//             // System.out.println(Arrays.toString(item));
+//             res.add(item[0]);
+//         }
+        
+//         return res;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[1] == b[1] ? b[0] - a[0] : b[1] - a[1];
+        });
+        for(int[] restaurant : restaurants) {
+            if((veganFriendly == 0 || veganFriendly == restaurant[2]) &&
+                restaurant[3] <= maxPrice &&
+                restaurant[4] <= maxDistance
+              ) {
+                pq.offer(restaurant);
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        while(!pq.isEmpty()) {
+            result.add(pq.poll()[0]);
+        }
+        return result;
+    }
+    
 }
-​
+
 class Comp implements Comparator<int[]> {
-    @Override
-    public int compare(int[] a, int[] b) {
-        if (b[1] == a[1])
-            return b[0] - a[0];
-        return b[1] - a[1];
-    }
+    @Override
+    public int compare(int[] a, int[] b) {
+        if (b[1] == a[1])
+            return b[0] - a[0];
+        return b[1] - a[1];
+    }
 }
